32b1bd8ec6134501f6b43f983af65100b6b0196a
jissmon jose <jissmon.jose@naicoits.com>
2021-09-30 09:11:12 +0530

    where clause filtering

--- DATABASE-BASICS/queries/3_WHERE_CLAUSE_AND_OR_OPERATOR.sql
+++ DATABASE-BASICS/queries/3_WHERE_CLAUSE_AND_OR_OPERATOR.sql
@@ -11,3 +11,13 @@ SELECT * FROM employees WHERE hire_date > 2010-01-01 AND gender = 'M';
 # Multiple conditions using AND operator.
 SELECT * FROM employees WHERE department='Computers' AND salary > 50000 AND region_id=3;
 
+# USING OR OPERATOR
+SELECT * FROM employees WHERE department='Clothing' OR last_name LIKE 'M%';
+
+-- above query returns rows where department is clothing or last name starts with letter 'M'.
+-- rows that satisfies either of both are returned.
+
+# select employees with salary>40000 and works in either clothing or pharmacy dept.
+
+SELECT * FROM employees WHERE salary > 40000 AND (department='Clothing' OR department='Pharmacy');
+
